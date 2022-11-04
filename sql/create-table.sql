b8a6900160e84ef377bcae4e15fccd85c1e76194
thonghp <thonghoangpham123@gmail.com>
2022-11-04 12:17:40 +0700

    add table creation query

---
+++ sql/create-table.sql
@@ -0,0 +1,195 @@
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `first_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `last_name` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `phone_number` varchar(10) NOT NULL,
+  `address` varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `image` varchar(64) DEFAULT NULL,
+  `email` varchar(128) NOT NULL,
+  `password_id` int NOT NULL,
+  `enabled` bit(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  KEY `FK_user_password_idx` (`password_id`),
+  CONSTRAINT `fk_user_password` FOREIGN KEY (`password_id`) REFERENCES `passwords` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `roles` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `description` varchar(150) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `user_role` (
+  `user_id` int NOT NULL,
+  `role_id` int NOT NULL,
+  PRIMARY KEY (`user_id`,`role_id`),
+  KEY `fk_role_idx` (`role_id`),
+  KEY `fk_user_idx` (`user_id`),
+  CONSTRAINT `fk_role` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`),
+  CONSTRAINT `fk_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `passwords` (
+  `id` int NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `created_time` varchar(45) NOT NULL,
+  `reset_password_token` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `customers` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `phone_number` varchar(10) DEFAULT NULL,
+  `address` varchar(256) DEFAULT NULL,
+  `email` varchar(128) NOT NULL,
+  `password_id` int NOT NULL,
+  `created_time` datetime NOT NULL,
+  `authentication_type` varchar(10) NOT NULL,
+  `verification_code` varchar(64) DEFAULT NULL,
+  `enabled` bit(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  KEY `fk_customer_password_idx` (`password_id`),
+  CONSTRAINT `fk_customer_password` FOREIGN KEY (`password_id`) REFERENCES `passwords` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `banners` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `image` varchar(255) NOT NULL,
+  `name` varchar(256) NOT NULL,
+  `alias` varchar(256) NOT NULL,
+  `enabled` bit(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `brands` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `image` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `categories` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `alias` varchar(45) NOT NULL,
+  `parent_id` int DEFAULT NULL,
+  `all_parent_ids` varchar(45) DEFAULT NULL,
+  `enabled` bit(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name_UNIQUE` (`name`),
+  KEY `fk_category_idx` (`parent_id`),
+  CONSTRAINT `fk_category` FOREIGN KEY (`parent_id`) REFERENCES `categories` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `brand_category` (
+  `brand_id` int NOT NULL,
+  `category_id` int NOT NULL,
+  PRIMARY KEY (`brand_id`,`category_id`),
+  KEY `fk_category_idx` (`category_id`),
+  CONSTRAINT `fk_brand_category` FOREIGN KEY (`brand_id`) REFERENCES `brands` (`id`),
+  CONSTRAINT `fk_category_brand` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `products` (
+  `id` int NOT NULL,
+  `main_image` varchar(255) NOT NULL,
+  `name` varchar(256) NOT NULL,
+  `alias` varchar(256) NOT NULL,
+  `brand_id` int NOT NULL,
+  `category_id` int NOT NULL,
+  `short_description` text,
+  `full_description` text,
+  `created_time` datetime NOT NULL,
+  `updated_time` datetime DEFAULT NULL,
+  `price` float NOT NULL,
+  `percent_discount` float DEFAULT '0',
+  `quantity` int NOT NULL,
+  `status` bit(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name_UNIQUE` (`name`),
+  KEY `fk_product_brand_idx` (`brand_id`),
+  KEY `fk_product_category_idx` (`category_id`),
+  CONSTRAINT `fk_product_brand` FOREIGN KEY (`brand_id`) REFERENCES `brands` (`id`),
+  CONSTRAINT `fk_product_category` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `product_details` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `value` varchar(255) NOT NULL,
+  `product_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_product_detail_idx` (`product_id`),
+  CONSTRAINT `fk_product_detail` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `product_images` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `image` varchar(255) NOT NULL,
+  `product_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_product_image_idx` (`product_id`),
+  CONSTRAINT `fk_product_image` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `cart_items` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `product_id` int NOT NULL,
+  `quantity` int NOT NULL,
+  `customer_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_cart_product_idx` (`product_id`),
+  KEY `fk_cart_customer_idx` (`customer_id`),
+  CONSTRAINT `fk_cart_customer` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`),
+  CONSTRAINT `fk_cart_product` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `orders` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `customer_id` int NOT NULL,
+  `order_time` datetime NOT NULL,
+  `payment_method` bit(1) NOT NULL,
+  `price` float NOT NULL,
+  `discount` float NOT NULL,
+  `tax` float NOT NULL,
+  `total` float NOT NULL,
+  `delivery_form` bit(1) NOT NULL,
+  `status_order` bit(5) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_order_customer_idx` (`customer_id`),
+  CONSTRAINT `fk_order_customer` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `order_details` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `order_id` int NOT NULL,
+  `product_id` int NOT NULL,
+  `quantity` int NOT NULL,
+  `product_cost` float NOT NULL,
+  `total` float NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_order_detail_idx` (`order_id`),
+  KEY `fk_order_product_idx` (`product_id`),
+  CONSTRAINT `fk_order_detail` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
+  CONSTRAINT `fk_order_product` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `settings` (
+  `key` varchar(128) NOT NULL,
+  `value` varchar(1024) NOT NULL,
+  `category` varchar(45) NOT NULL,
+  PRIMARY KEY (`key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+
+
+
+
+
