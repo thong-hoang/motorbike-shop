0ff223105f66334724197e51984ea00a9b96a133
thonghp <thonghoangpham123@gmail.com>
2022-11-27 22:49:18 +0700

    test: code unit tests for RoleDAO

--- src/test/java/edu/nlu/motorbike_shop/dao/RoleDAOTest.java
+++ src/test/java/edu/nlu/motorbike_shop/dao/RoleDAOTest.java
@@ -26,4 +26,47 @@ public class RoleDAOTest {
 
         assertTrue(roleDAO.save(role));
     }
+
+    @Test
+    public void testFindRoleByName() {
+        String nameExists = "Quản trị viên";
+        String nameNotExists = "Khách hàng";
+
+        Role admin = roleDAO.findByName(nameExists);
+        Role customer = roleDAO.findByName(nameNotExists);
+
+        assertNotNull(admin);
+        assertNull(customer);
+    }
+
+    @Test
+    public void testFindRoleById() {
+        Integer id = 1;
+        Role role = roleDAO.findById(id);
+
+        assertNotNull(role);
+    }
+
+    @Test
+    public void testUpdateRole() {
+        Role role = roleDAO.findByName("Quản trị viên");
+        role.setName("Nhân viên bán hàng");
+        role.setDescription("Quản lý sản phẩm");
+
+        roleDAO.update(role);
+
+        assertEquals("Nhân viên bán hàng", role.getName());
+        assertEquals("Quản lý sản phẩm", role.getDescription());
+    }
+
+    @Test
+    public void testDeleteRole() {
+        Integer id = 1;
+
+        roleDAO.delete(id);
+
+        Role role = roleDAO.findById(1);
+
+        assertNull(role);
+    }
 }
