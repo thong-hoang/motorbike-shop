668714e624f398fbc7eba86418470c31ba262432
thonghp <thonghoangpham123@gmail.com>
2022-12-12 11:20:16 +0700

    add address, employee entity and add constructor role

---
+++ src/main/java/edu/nlu/motorbike_shop/entity/Employee.java
@@ -0,0 +1,69 @@
+package edu.nlu.motorbike_shop.entity;
+
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+public class Employee {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String imagePath;
+    private String email;
+    private String password;
+    private boolean enabled;
+    private Set<Role> roles = new HashSet<>();
+    private Address address;
+
+    public Employee(Integer id, String firstName, String lastName, String phoneNumber, Address address, String imagePath,
+                    String email, boolean enabled) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.imagePath = imagePath;
+        this.email = email;
+        this.enabled = enabled;
+    }
+
+    public Employee(String firstName, String lastName, String phoneNumber, Address address, String imagePath, String email,
+                    String password, boolean enabled) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.imagePath = imagePath;
+        this.email = email;
+        this.password = password;
+        this.enabled = enabled;
+    }
+
+    public Employee(Integer id, String firstName, String lastName, String phoneNumber, Address address, String imagePath,
+                    String email, String password, boolean enabled) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.imagePath = imagePath;
+        this.email = email;
+        this.password = password;
+        this.enabled = enabled;
+    }
+
+    public void addRole(Role role) {
+        this.roles.add(role);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" + "id='" + id + '\'' + ", firstName='" + firstName + '\'' + ", lastName='" + lastName + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' + ", address='" + address + '\'' + ", imagePath='" + imagePath + '\'' +
+                ", email='" + email + '\'' + ", password='" + password + '\'' + ", enabled=" + enabled + ", role='" + roles + '}';
+    }
+}
--- src/main/java/edu/nlu/motorbike_shop/entity/Role.java
+++ src/main/java/edu/nlu/motorbike_shop/entity/Role.java
@@ -12,6 +12,11 @@ public class Role {
     private String name;
     private String description;
 
+    public Role(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public Role(String name, String description) {
         this.name = name;
         this.description = description;
