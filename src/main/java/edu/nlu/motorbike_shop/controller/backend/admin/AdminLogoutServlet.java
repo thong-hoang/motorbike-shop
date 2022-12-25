e1d80d179e4a3f5cba1f434cbcf94aa2046ffaa8
thonghp <thonghoangpham123@gmail.com>
2022-12-25 16:57:34 +0700

    add removeAttribute of image session

--- src/main/java/edu/nlu/motorbike_shop/controller/backend/admin/AdminLogoutServlet.java
+++ src/main/java/edu/nlu/motorbike_shop/controller/backend/admin/AdminLogoutServlet.java
@@ -10,6 +10,7 @@ public class AdminLogoutServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
+        session.removeAttribute("id");
         session.removeAttribute("email");
         session.removeAttribute("firstName");
         session.removeAttribute("lastName");
