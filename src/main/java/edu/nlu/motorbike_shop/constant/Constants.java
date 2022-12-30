7e4c83316ec17f222904c7bfb62a4c2ffe8b3fcc
Hoàng Vinh <73897533+Hoang-Vinh-892@users.noreply.github.com>
2022-12-31 02:04:03 +0700

    update constructor Customer Entity

---
+++ src/main/java/edu/nlu/motorbike_shop/constant/Constants.java
@@ -0,0 +1,10 @@
+package edu.nlu.motorbike_shop.constant;
+
+public final class Constants {
+    private Constants() {
+    }
+    public static final String DEFAULT_SORT_TYPE = "ASC";
+    public static final String DEFAULT_SORT_FIELD = "id";
+    public static final int DEFAULT_PAGE_SIZE = 5;
+    public static final String DEFAULT_IMAGE_DIRECTORY = "images";
+}
