54da00a23f805d4e6515b6977ca51a9a33929c38
thonghp <thonghoangpham123@gmail.com>
2022-12-31 22:38:10 +0700

    add banner data sql

---
+++ sql/banner-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO `banners` (`id`, `image_path`, `name`, `created_time`, `enabled`)
+VALUES (1, 'elite.jpg', 'Vui cùng elite', '2022-12-31 21:32:53', b'1'),
+       (2, 'grande.jpg', 'Grande thế hệ mới', '2022-12-31 21:35:39', b'1'),
+       (3, 'motor-r15.png', 'Chiến cùng motor r15', '2022-12-31 21:35:57', b'1');
