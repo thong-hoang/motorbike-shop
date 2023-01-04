0258fccdc0173679e261c46cbc0713d8a8cd010c
T-Vinh <18130286@st.hcmuaf.edu.vn>
2023-01-04 11:11:40 +0700

    feat: add function update category

---
+++ src/main/java/edu/nlu/motorbike_shop/controller/backend/category/UpdateCategoryServlet.java
@@ -0,0 +1,17 @@
+package edu.nlu.motorbike_shop.controller.backend.category;
+
+import edu.nlu.motorbike_shop.service.CategoryService;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "UpdateCategoryServlet", value = "/backend/update_category")
+public class UpdateCategoryServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        CategoryService service = new CategoryService(request, response);
+        service.updateCategory();
+    }
+}
