b419cc0d403461be6866aaf00f4b48e0814a7b39
thonghp <thonghoangpham123@gmail.com>
2023-01-06 21:46:32 +0700

    add brand category data sql

--- sql/data-brand-category.sql
+++ sql/data-brand-category.sql
@@ -4,9 +4,9 @@ VALUES (1, 'Honda', 'honda.jpg', b'1'),
        (2, 'Yamaha', 'yamaha.png', b'1'),
        (3, 'Piaggio', 'piaggio.png', b'1'),
        (4, 'Sym', 'sym.png', b'1'),
-       (5, 'Suzuki', NULL, b'1'),
-       (6, 'Ducati', NULL, b'1'),
-       (7, 'Kawasaki', NULL, b'1');
+       (5, 'Suzuki', 'suzuki.png', b'1'),
+       (6, 'Ducati', 'ducati.png', b'1'),
+       (7, 'Kawasaki', 'kawasaki.png', b'1');
 
 -- category
 INSERT INTO `categories` (`id`, `name`, `alias`, `parent_id`, `enabled`)
@@ -15,3 +15,21 @@ VALUES (1, 'xe máy', '', NULL, b'1'),
        (3, 'xe côn tay', 'xe-côn-tay', 1, b'1'),
        (4, 'xe tay ga', 'xe-tay-ga', 1, b'1'),
        (5, 'xe mô tô', 'xe-mô-tô', 1, b'1');
+
+-- brand_category
+INSERT INTO `brand_category` (`brand_id`, `category_id`)
+VALUES (1, 2),
+       (1, 3),
+       (1, 4),
+       (1, 5),
+       (2, 2),
+       (2, 3),
+       (2, 4),
+       (2, 5),
+       (3, 4),
+       (4, 2),
+       (5, 2),
+       (5, 3),
+       (5, 5),
+       (6, 5),
+       (7, 5);
